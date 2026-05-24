8a15efff2e444686bbd5eb0a868e67d8362a640e
iggy <macetaconagua@gmail.com>
2026-05-24 16:51:25 -0300

    1.21.1 Change

---
+++ src/main/java/com/iggy/iggytech/Blocks/ConveyorBeltBlock.java
@@ -0,0 +1,45 @@
+package com.iggy.iggytech.Blocks;
+
+import com.mojang.serialization.MapCodec;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.HorizontalDirectionalBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public class ConveyorBeltBlock extends HorizontalDirectionalBlock {
+
+    public static final MapCodec<ConveyorBeltBlock> CODEC = simpleCodec(ConveyorBeltBlock::new);
+
+    private static final VoxelShape SHAPE = Block.box(0, 0, 0, 16, 3, 16);
+
+    @Override
+    protected @NotNull VoxelShape getShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context) {
+        return SHAPE;
+    }
+
+    public ConveyorBeltBlock(Properties properties) {
+        super(properties);
+    }
+
+    @Override
+    protected MapCodec<? extends HorizontalDirectionalBlock> codec() {
+        return CODEC;
+    }
+
+    @Override
+    public @Nullable BlockState getStateForPlacement(BlockPlaceContext context) {
+        return this.defaultBlockState().setValue(FACING, context.getHorizontalDirection());
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        builder.add(FACING);
+    }
+}
---
+++ src/main/java/com/iggy/iggytech/iggytech.java
@@ -0,0 +1,52 @@
+package com.iggy.iggytech;
+
+import com.iggy.iggytech.Blocks.ConveyorBeltBlock;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.level.block.Block;
+import net.neoforged.neoforge.registries.DeferredBlock;
+import net.neoforged.neoforge.registries.DeferredItem;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.state.BlockBehaviour.Properties;
+import net.neoforged.neoforge.registries.DeferredRegister;
+import org.slf4j.Logger;
+
+import com.mojang.logging.LogUtils;
+
+
+import net.neoforged.bus.api.IEventBus;
+
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.config.ModConfig;
+import net.neoforged.fml.ModContainer;
+
+
+import net.neoforged.neoforge.common.NeoForge;
+
+
+// The value here should match an entry in the META-INF/neoforge.mods.toml file
+@Mod(iggytech.MODID)
+public class iggytech {
+    // Define mod id in a common place for everything to reference
+    public static final String MODID = "iggytech";
+    public static final Logger LOGGER = LogUtils.getLogger();
+
+    public static final DeferredRegister.Blocks BLOCKS = DeferredRegister.createBlocks(MODID);
+    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(MODID);
+
+    public static final DeferredBlock<ConveyorBeltBlock> CONVEYOR_BELT = BLOCKS.registerBlock(
+            "conveyor_belt",
+            ConveyorBeltBlock::new,
+            BlockBehaviour.Properties.of().noOcclusion()
+    );
+
+    public static final DeferredItem<BlockItem> CONVEYOR_BELT_ITEM = ITEMS.registerSimpleBlockItem("conveyor_belt", CONVEYOR_BELT);
+
+    public iggytech(IEventBus modEventBus, ModContainer modContainer) {
+        // Register our mod's ModConfigSpec so that FML can create and load the config file for us
+        modContainer.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
+        BLOCKS.register(modEventBus);
+        ITEMS.register(modEventBus);
+    }
+}
+
+
